24728d599d2c6aa41cc76e894fef31bd5edb1ef3
niteeshchaudhary <nkchaudhary00@gmail.com>
2022-03-19 20:51:39 +0530

    searchbox

---
+++ client/src/components/Litems/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const LShowContainer = styled.div`
+  width: 100%;
+  min-height: 6em;
+  display: flex;
+  border-bottom: 2px solid #d8d8d852;
+  padding: 6px 8px;
+  align-items: center;
+`;
+
+const Thumbnail = styled.div`
+  width: auto;
+  height: 100%;
+  display: flex;
+  flex: 0.4;
+
+  img {
+    width: auto;
+    height: 100%;
+  }
+`;
+
+const Name = styled.h3`
+  font-size: 15px;
+  color: #000;
+  margin-left: 10px;
+  flex: 2;
+  display: flex;
+`;
+
+const Rating = styled.span`
+  color: #a1a1a1;
+  font-size: 16px;
+  display: flex;
+  flex: 0.2;
+`;
+
+export function Litem(props) {
+  const {Company} = props;
+
+  return (
+    <LShowContainer>
+      <Name>{Company}</Name>
+    </LShowContainer>
+  );
+}
