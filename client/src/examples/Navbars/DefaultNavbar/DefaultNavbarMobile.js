28a80d3bbe7e60db5ab3c8829864a9ff613d2caf
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2022-03-19 07:34:27 +0530

    Updated Frontend

--- client/src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
+++ client/src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
@@ -46,9 +46,9 @@ function DefaultNavbarMobile({ open, close }) {
     >
       <MDBox px={0.5}>
         <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" />
-        <DefaultNavbarLink icon="person" name="profile" route="/profile" />
-        <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" />
-        <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" />
+        {/* <DefaultNavbarLink icon="person" name="profile" route="/profile" /> */}
+        {/* <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" /> */}
+        {/* <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" /> */}
       </MDBox>
     </Menu>
   );
--- client/src/examples/Navbars/DefaultNavbar/index.js
+++ client/src/examples/Navbars/DefaultNavbar/index.js
@@ -115,19 +115,19 @@ function DefaultNavbar({ transparent, light, action }) {
         </MDBox>
         <MDBox color="inherit" display={{ xs: "none", lg: "flex" }} m={0} p={0}>
           <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" light={light} />
-          <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} />
-          <DefaultNavbarLink
+          {/* <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} /> */}
+          {/* <DefaultNavbarLink
             icon="account_circle"
             name="sign up"
             route="/authentication/sign-up"
             light={light}
-          />
-          <DefaultNavbarLink
+          /> */}
+          {/* <DefaultNavbarLink
             icon="key"
             name="sign in"
             route="/authentication/sign-in"
             light={light}
-          />
+          /> */}
         </MDBox>
         {action &&
           (action.type === "internal" ? (
