8af4bf8d68878407d1351e40e6392ceae595f732
harrithha <76431379+harrithha@users.noreply.github.com>
2022-03-19 15:11:17 +0530

    model_finished

--- ML_Model/nasdaq.py
+++ ML_Model/nasdaq.py
@@ -3,17 +3,15 @@ import time
 def market(ticker, start, end):
     start = time.strptime(start, "%Y-%m-%d")
     end = time.strptime(end, "%Y-%m-%d")
-    print(type(start))
-    print(type(end))
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='+ticker+'&apikey=UPGJXRX8G99R3BG5'
     r = requests.get(url)
     data = r.json()
     ans = []
-    for date in data["Weekly Time Series"]:
-        temp_date = time.strptime(date, "%Y-%m-%d")
-        if temp_date<=end and temp_date>=start:
-            ans.append([date, float(data["Weekly Time Series"][date]["4. close"])])
-    return ans
-
-tick = input()
-print(market(tick, input(), input()))
+    try:
+        for date in data["Weekly Time Series"]:
+            temp_date = time.strptime(date, "%Y-%m-%d")
+            if temp_date<=end and temp_date>=start:
+                ans.append([date, float(data["Weekly Time Series"][date]["4. close"])])
+        return ans
+    except:
+        return []
