ecdc20c3f0b15b3438f3e74a0c202684e66c2e69
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2022-03-19 10:35:50 +0530

    Redited the search bar

--- client/src/examples/Navbars/DashboardNavbar/index.js
+++ client/src/examples/Navbars/DashboardNavbar/index.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useDebounce, useDebouncedCallback } from 'use-debounce';
 
 // react-router components
 import { useLocation, Link } from "react-router-dom";
@@ -25,95 +24,111 @@ import NotificationItem from "examples/Items/NotificationItem";
 
 // Custom styles for DashboardNavbar
 import {
-    navbar,
-    navbarContainer,
-    navbarRow,
-    navbarIconButton,
-    navbarMobileMenu,
+  navbar,
+  navbarContainer,
+  navbarRow,
+  navbarIconButton,
+  navbarMobileMenu,
 } from "examples/Navbars/DashboardNavbar/styles";
 
 // Material Dashboard 2 React context
 import {
-    useMaterialUIController,
-    setTransparentNavbar,
-    setMiniSidenav,
-    setOpenConfigurator,
+  useMaterialUIController,
+  setTransparentNavbar,
+  setMiniSidenav,
+  setOpenConfigurator,
 } from "context";
 
 function DashboardNavbar({ absolute, light, isMini }) {
-    const [navbarType, setNavbarType] = useState();
-    const [controller, dispatch] = useMaterialUIController();
-    const { miniSidenav, transparentNavbar, fixedNavbar, openConfigurator, darkMode } = controller;
-    const [openMenu, setOpenMenu] = useState(false);
-    const route = useLocation().pathname.split("/").slice(1);
+  const [navbarType, setNavbarType] = useState();
+  const [controller, dispatch] = useMaterialUIController();
+  const { miniSidenav, transparentNavbar, fixedNavbar, openConfigurator, darkMode } = controller;
+  const [openMenu, setOpenMenu] = useState(false);
+  const route = useLocation().pathname.split("/").slice(1);
 
-    useEffect(() => {
-        // Setting the navbar type
-        if (fixedNavbar) {
-            setNavbarType("sticky");
-        } else {
-            setNavbarType("static");
-        }
+  useEffect(() => {
+    // Setting the navbar type
+    if (fixedNavbar) {
+      setNavbarType("sticky");
+    } else {
+      setNavbarType("static");
+    }
 
-        // A function that sets the transparent state of the navbar.
-        function handleTransparentNavbar() {
-            setTransparentNavbar(dispatch, (fixedNavbar && window.scrollY === 0) || !fixedNavbar);
-        }
+    // A function that sets the transparent state of the navbar.
+    function handleTransparentNavbar() {
+      setTransparentNavbar(dispatch, (fixedNavbar && window.scrollY === 0) || !fixedNavbar);
+    }
 
-        /** 
+    /** 
          The event listener that's calling the handleTransparentNavbar function when 
          scrolling the window.
         */
-        window.addEventListener("scroll", handleTransparentNavbar);
+    window.addEventListener("scroll", handleTransparentNavbar);
 
-        // Call the handleTransparentNavbar function to set the state with the initial value.
-        handleTransparentNavbar();
+    // Call the handleTransparentNavbar function to set the state with the initial value.
+    handleTransparentNavbar();
 
-        // Remove event listener on cleanup
-        return () => window.removeEventListener("scroll", handleTransparentNavbar);
-    }, [dispatch, fixedNavbar]);
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("scroll", handleTransparentNavbar);
+  }, [dispatch, fixedNavbar]);
 
-    const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
-    const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
-    const handleOpenMenu = (event) => setOpenMenu(event.currentTarget);
-    const handleCloseMenu = () => setOpenMenu(false);
+  const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+  const handleOpenMenu = (event) => setOpenMenu(event.currentTarget);
+  const handleCloseMenu = () => setOpenMenu(false);
 
-    // Render the notifications menu
-    const renderMenu = () => ( <
-            Menu anchorEl = { openMenu }
-            anchorReference = { null }
-            anchorOrigin = {
-                {
-                    vertical: "bottom",
-                    horizontal: "left",
-                }
-            }
-            open = { Boolean(openMenu) }
-            onClose = { handleCloseMenu }
-            sx = {
-                { mt: 2 } } >
-            <
-            NotificationItem icon = { < Icon > email < /Icon>} title="Check new messages" / >
-                <
-                NotificationItem icon = { < Icon > podcasts < /Icon>} title="Manage Podcast sessions" / >
-                    <
-                    NotificationItem icon = { < Icon > shopping_cart < /Icon>} title="Payment successfully completed" / >
-                        <
-                        /Menu>
-                    );
+  // Render the notifications menu
+  const renderMenu = () => (
+    <Menu
+      anchorEl={openMenu}
+      anchorReference={null}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      open={Boolean(openMenu)}
+      onClose={handleCloseMenu}
+      sx={{ mt: 2 }}
+    >
+      <NotificationItem icon={<Icon> email </Icon>} title="Check new messages" />
+      <NotificationItem icon={<Icon> podcasts </Icon>} title="Manage Podcast sessions" />
+      <NotificationItem
+        icon={<Icon> shopping_cart </Icon>}
+        title="Payment successfully completed"
+      />
+    </Menu>
+  );
 
-                    // Styles for the navbar icons
-                    const iconsStyle = ({ palette: { dark, white, text }, functions: { rgba } }) => ({
-                        color: () => {
-                            let colorValue = light || darkMode ? white.main : dark.main;
+  // Styles for the navbar icons
+  const iconsStyle = ({ palette: { dark, white, text }, functions: { rgba } }) => ({
+    color: () => {
+      let colorValue = light || darkMode ? white.main : dark.main;
 
-                            if (transparentNavbar && !light) {
-                                colorValue = darkMode ? rgba(text.main, 0.6) : text.main;
-                            }
+      if (transparentNavbar && !light) {
+        colorValue = darkMode ? rgba(text.main, 0.6) : text.main;
+      }
 
-                            return colorValue;
-                        },
-                    });
+      return colorValue;
+    },
+  });
+
+  function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+  }
+
+  function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
 
     var costs = new Array();
     for (var i = 0; i <= s1.length; i++) {
@@ -147,7 +162,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
     return dist;
   };
   const [searchValue, setSearchValue] = useState("");
-  const [searchText] = useDebounce(searchValue, 500);
   const [comp, setComp] = useState([]);
   // useEffect(() => {
   //   console.log(searchValue);
@@ -155,32 +169,18 @@ function DashboardNavbar({ absolute, light, isMini }) {
   // const handleSearchValue = (text) => {
   //   setSearchValue(text.target.values);
   // };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    var li = [];
+    compData.forEach((el) => {
+      var sim = similarity(`${searchValue}`, `${el.Company}`);
+      if (sim >= 0.3 || el.Company.includes(searchValue)) {
+        li.push(el);
+      }
+    });
 
-  const debounced = useDebouncedCallback(
-    // function
-    (value) => {
-      setSearchValue(value);
-    },
-    // delay in ms
-    1000
-  );
-
-  const onSearch = (e) => {
-    // e.preventDefault();
-
-    debounced(e.target.value);
-    setTimeout(() => {
-      var li = [];
-      compData.forEach((el) => {
-        var sim = similarity(`${searchValue}`, `${el.Company}`);
-        if (sim >= 0.3 || el.Company.includes(searchValue)) {
-          li.push(el);
-        }
-      });
-
-      console.log(li);
-      setComp(li);
-    }, 4000);
+    console.log(li);
+    setComp(li);
   };
 
   return (
@@ -191,40 +191,37 @@ function DashboardNavbar({ absolute, light, isMini }) {
     >
       <Toolbar sx={(theme) => navbarContainer(theme)}>
         <MDBox color="inherit" mb={{ xs: 1, md: 0 }} sx={(theme) => navbarRow(theme, { isMini })}>
-          <Breadcrumbs icon="home" title={route[route.length - 1]} route={route} light={light} />
-        </MDBox>
+          <Breadcrumbs icon="home" title={route[route.length - 1]} route={route} light={light} />{" "}
+        </MDBox>{" "}
         {isMini ? null : (
           <MDBox sx={(theme) => navbarRow(theme, { isMini })}>
             <MDBox pr={1}>
-              <MDInput
-                label="Search here "
-                type="text"
-                value={searchValue}
-                onChange={onSearch}
-              />
-              {/* <form action="submit" onSubmit={onSubmit}>
+              {" "}
+              {/* <MDInput label="Search here " /> */}{" "}
+              <form action="submit" onSubmit={onSubmit}>
                 <input type="submit" hidden />
                 <input
                   type="text"
                   value={searchValue}
                   onChange={(e) => setSearchValue(e.target.value)}
-                />
+                />{" "}
                 <button
                   type="submit"
                   onClick={(e) => {
                     e.preventDefault();
                   }}
                 >
-                  Search
-                </button>
-              </form> */}
-            </MDBox>
+                  Search{" "}
+                </button>{" "}
+              </form>{" "}
+            </MDBox>{" "}
             <MDBox color={light ? "white" : "inherit"}>
+              {" "}
               {/* <Link to="/authentication/sign-in/basic">
-                <IconButton sx={navbarIconButton} size="small" disableRipple>
-                  <Icon sx={iconsStyle}>account_circle</Icon>
-                </IconButton>
-              </Link> */}
+                                                  <IconButton sx={navbarIconButton} size="small" disableRipple>
+                                                    <Icon sx={iconsStyle}>account_circle</Icon>
+                                                  </IconButton>
+                                                </Link> */}{" "}
               <IconButton
                 size="small"
                 disableRipple
@@ -233,9 +230,10 @@ function DashboardNavbar({ absolute, light, isMini }) {
                 onClick={handleMiniSidenav}
               >
                 <Icon sx={iconsStyle} fontSize="medium">
-                  {miniSidenav ? "menu_open" : "menu"}
-                </Icon>
-              </IconButton>
+                  {" "}
+                  {miniSidenav ? "menu_open" : "menu"}{" "}
+                </Icon>{" "}
+              </IconButton>{" "}
               <IconButton
                 size="small"
                 disableRipple
@@ -243,41 +241,41 @@ function DashboardNavbar({ absolute, light, isMini }) {
                 sx={navbarIconButton}
                 onClick={handleConfiguratorOpen}
               >
-                <Icon sx={iconsStyle}>settings</Icon>
-              </IconButton>
+                <Icon sx={iconsStyle}> settings </Icon>{" "}
+              </IconButton>{" "}
               {/* <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarIconButton}
-                aria-controls="notification-menu"
-                aria-haspopup="true"
-                variant="contained"
-                onClick={handleOpenMenu}
-              >
-                <Icon sx={iconsStyle}>notifications</Icon>
-              </IconButton> */}
-              {/* {renderMenu()} */}
-            </MDBox>
+                                                  size="small"
+                                                  disableRipple
+                                                  color="inherit"
+                                                  sx={navbarIconButton}
+                                                  aria-controls="notification-menu"
+                                                  aria-haspopup="true"
+                                                  variant="contained"
+                                                  onClick={handleOpenMenu}
+                                                >
+                                                  <Icon sx={iconsStyle}>notifications</Icon>
+                                                </IconButton> */}{" "}
+              {/* {renderMenu()} */}{" "}
+            </MDBox>{" "}
           </MDBox>
-        )}
-      </Toolbar>
+        )}{" "}
+      </Toolbar>{" "}
     </AppBar>
   );
 }
 
-                // Setting default values for the props of DashboardNavbar
-                DashboardNavbar.defaultProps = {
-                    absolute: false,
-                    light: false,
-                    isMini: false,
-                };
+// Setting default values for the props of DashboardNavbar
+DashboardNavbar.defaultProps = {
+  absolute: false,
+  light: false,
+  isMini: false,
+};
 
-                // Typechecking props for the DashboardNavbar
-                DashboardNavbar.propTypes = {
-                    absolute: PropTypes.bool,
-                    light: PropTypes.bool,
-                    isMini: PropTypes.bool,
-                };
+// Typechecking props for the DashboardNavbar
+DashboardNavbar.propTypes = {
+  absolute: PropTypes.bool,
+  light: PropTypes.bool,
+  isMini: PropTypes.bool,
+};
 
-                export default DashboardNavbar;
+export default DashboardNavbar;
