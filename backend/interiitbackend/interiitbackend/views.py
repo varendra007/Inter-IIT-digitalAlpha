6d8294eeafffdf44caf4c347f1234484ab8f9f32
Ashish Akash <ashishakash333@gmail.com>
2022-03-19 19:50:25 +0530

    added django

---
+++ backend/interiitbackend/interiitbackend/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+import json
+import sys
+import os
+from django.conf import settings
+path=sys.path[0].split('\\')
+for i in range(len(path)):
+    if path[i]=='Inter-IIT-digitalAlpha':
+        break
+sys.path.append('/'.join(path[:i+1]))
+#print(sys.path)
+from ML_Model.final import predict_progress
+
+def index(request):
+    data = json.loads(request.data)
+    arrofstr=data['arr']
+    responseData=json.dumps(predict_progress(arrofstr))
+    return HttpResponse(json.dumps(responseData), content_type="application/json")
