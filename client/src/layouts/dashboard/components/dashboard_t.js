138f2a1ba1ed66215057a0e182130b8a7ebb5359
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2022-03-25 13:05:44 +0530

    Added API calls in the frontrend

--- client/src/layouts/dashboard/components/dashboard_t.js
+++ client/src/layouts/dashboard/components/dashboard_t.js
@@ -1,4 +1,5 @@
 import Grid from "@mui/material/Grid";
+import axios from 'axios';
 
 import MDBox from "components/MDBox";
 
@@ -18,20 +19,35 @@ import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatist
 // import Projects from "layouts/dashboard/components/Projects";
 // import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
 import json1 from "../data/10qjson1.json";
-import json2 from "../data/10qjson2.json";
-import json3 from "../data/10qjson3.json";
-import json4 from "../data/10qjson4.json";
-import json5 from "../data/10qjson5.json";
+// import json2 from "../data/10qjson2.json";
+// import json3 from "../data/10qjson3.json";
+// import json4 from "../data/10qjson4.json";
+// import json5 from "../data/10qjson5.json";
 import { useEffect, useState } from "react";
 
 function Dashboard_t() {
   // const { Income, Revenue, Expense, Loss, Stock,NetIncome } = reportsLineChartData;
-  const arr = [json1, json2, json3, json4, json5]
-  const [file, setFile] = useState(json1);
+  // const arr = [json1, json2, json3, json4, json5]
+  // const [file, setFile] = useState(json1);
+  const [reqBody, setReqBody] = useState(json1);
+  const [search, setSearchQuery] = useState(JSON.parse(localStorage.getItem("searchCom")));
   useEffect(() => {
-    let x = Math.floor(Math.random() * 5 );
-    setFile(arr[x]);
-  }, []);
+    // let x = Math.floor(Math.random() * 5 );
+    // setFile(arr[x]);
+    try {
+      console.log(search);
+      axios(
+        `http://localhost:5000/data/predict10q?companies=${search.company.Ticker}&startDate=${search.startDate}&endDate=${search.endDate}`
+      ).then((res) => {
+        setReqBody(res.data?.data);
+        console.log(res.data?.data);
+      }).catch((err) => console.log(err));
+      // setTimeout(() => {
+      // }, 1000);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [search]);
 
   return (
     <DashboardLayout>
@@ -40,7 +56,7 @@ function Dashboard_t() {
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
             {" "}
-            {file.results.map((el, ind) => {
+            {reqBody?.result?.map((el, ind) => {
               return (
                 <>
                   <Grid item xs={12} md={6} lg={4}>
--- client/src/layouts/tables/index.js
+++ client/src/layouts/tables/index.js
@@ -1,6 +1,8 @@
 // @mui material components
+import React, { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
+import axios from 'axios';
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -15,50 +17,70 @@ import json from "../dashboard/data/10kjson1.json";
 function Tables() {
     var grwth=["Will Fall","Will remain Neutral","Will Rise"];
     var clr=["red","blue","green"];
-    var ind=1-1;
+    const [ind, setInd] = useState(0);
+    const [reqBody, setReqBody] = useState(json);
+    const [search, setSearchQuery] = useState(JSON.parse(localStorage.getItem("searchCom")));
 
-    return ( <DashboardLayout >
-        <DashboardNavbar / >
-        <MDBox pt = { 6 }
-        pb = { 3 } >
-        <Grid container spacing = { 6 } >
-            <Grid item xs = { 12 } >
-                <Card >
-                    <MDBox mx = { 2 } mt = {-3 } py = { 3 } px = { 2 } variant = "gradient" bgColor = "info" borderRadius = "lg" coloredShadow = "info" >
-                        <MDTypography variant = "h6" color = "white" >Prediction</MDTypography> 
-                    </MDBox> 
-                </Card> 
-            </Grid>
-            <Grid item xs = { 6 } >
-                <Card >
-                    {
-                        json.results.map((el, ind) => {
-                            if(el.head=="Stock Value"){
-                                return (
-                                        <MDBox mb={3}>
-                                            <ReportsLineChart
-                                                color="info"
-                                                title={el.head}
-                                                chart={{
-                                                labels: el.time,
-                                                datasets: el.y,
-                                                }}
-                                            />
-                                        </MDBox>
-                                    )
-                            }
-                        })
-                    }
+    useEffect(() => {
+        try {
+            console.log(search);
+            axios(
+              `http://localhost:5000/data/search?companies=${Number(search.company.CIK)}&startDate=${search.startDate}&endDate=${search.endDate}`
+            ).then((res) => {
+              setReqBody(Object.values(res.data?.data['8-k']));
+              console.log(Object.values(res.data?.data['8-k']));
+              setInd(Object.values(res.data?.data['8-k']));
+            }).catch((err) => console.log(err));
+            // setTimeout(() => {
+            // }, 1000);
+          } catch (error) {
+            console.log(error);
+          }
+    }, [search]);
 
-                </Card> 
+    return ( 
+        <DashboardLayout >
+            <DashboardNavbar / >
+            <MDBox pt = { 6 }
+            pb = { 3 } >
+            <Grid container spacing = { 6 } >
+                <Grid item xs = { 12 } >
+                    <Card >
+                        <MDBox mx = { 2 } mt = {-3 } py = { 3 } px = { 2 } variant = "gradient" bgColor = "info" borderRadius = "lg" coloredShadow = "info" >
+                            <MDTypography variant = "h6" color = "white" >Prediction</MDTypography> 
+                        </MDBox> 
+                    </Card> 
+                </Grid>
+                <Grid item xs = { 6 } >
+                    <Card >
+                        {
+                            reqBody?.map((el, ind) => {
+                                if(el.head==="Stock Value"){
+                                    return (
+                                            <MDBox mb={3}>
+                                                <ReportsLineChart
+                                                    color="info"
+                                                    title={el.head}
+                                                    chart={{
+                                                    labels: el.time,
+                                                    datasets: el.y,
+                                                    }}
+                                                />
+                                            </MDBox>
+                                        )
+                                }
+                            })
+                        }
+
+                    </Card> 
+                </Grid> 
             </Grid> 
-        </Grid> 
-        </MDBox> 
-        <MDBox pt = { 6 }
-        pb = { 3 } >
-            <h4>Market :</h4>
-            <h4><font color={clr[ind]}>{grwth[ind]}</font></h4>
-        </MDBox>
+            </MDBox> 
+            <MDBox pt = { 6 }
+            pb = { 3 } >
+                <h4>Market :</h4>
+                <h4><font color={clr[ind]}>{grwth[ind]}</font></h4>
+            </MDBox>
         </DashboardLayout>
     );
 }
